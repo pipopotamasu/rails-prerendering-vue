5cfb1793402b622f52cebef1bdc672f315bad172
pipopotamasu <ec0156hx@gmail.com>
2018-01-16 20:47:16 +0900

    add ajax

--- app/javascript/components/App.vue
+++ app/javascript/components/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <ul>
+      <li v-for="country in countries">
+        {{ country.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+// import countries from '../countries.json'
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      countries: []
     }
+  },
+  created: function() {
+    let self = this;
+    axios.get('https://restcountries.eu/rest/v2/all').then(res => {
+      self.countries = res.data;
+    });
   }
 }
 </script>
