0faf428c8f8a33f3038df446fffc5616f322c30d
pipopotamasu <ec0156hx@gmail.com>
2018-01-16 20:23:45 +0900

    add vue component

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,12 @@
 const { environment } = require('@rails/webpacker')
+const merge = require('webpack-merge')
 const vue =  require('./loaders/vue')
 
 environment.loaders.append('vue', vue)
-module.exports = environment
+module.exports = merge(environment.toWebpackConfig(), {
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  }
+})
